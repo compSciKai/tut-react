acb9c8c6c175b11b2816264955342a7b813913a4
compscikai <kaigra@icloud.com>
2021-01-24 23:15:12 -0800

    exercise for loading data async

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 
-const [firstItemInArray] = ["boots", "tent", "headlamp"];
-console.log(firstItemInArray);
-
-ReactDOM.render(<App authorized={true}/>, document.getElementById('root')
+ReactDOM.render(<App login="compscikai"/>, document.getElementById('root')
 );
