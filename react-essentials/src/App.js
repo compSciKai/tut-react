9759c554d93f1d202762770a6fc75c8f173c0e6e
compscikai <kaigra@icloud.com>
2021-01-24 22:57:02 -0800

    incorporated useReducer

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  );
 
   return (
       <>
-        <h1>Current emotion is {emotion} and {secondary}</h1>
-        <button onClick={() => setEmotion("happy")}>Make Happy</button>
-        <button onClick={() => setEmotion("frustrated")}>Make Frustrate</button>
-        <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
-        <button onClick={() => setEmotion("enthusiastic")}>Make Enthuse</button>
+        <input 
+          type="checkbox" 
+          value={checked} 
+          onChange={toggle}
+        />
+        <p>{checked ? "checked" : "not checked"}</p>
       </>
   )
 }
